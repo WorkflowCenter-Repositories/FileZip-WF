6bcf87fa2b21edf5b385730fbd2eb65328e8244c
Rawaa Qasha <r.qasha@newcastle.ac.uk>
2016-04-05 14:47:28 +0100

    Update image_create.sh

--- scripts/image_create.sh
+++ scripts/image_create.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 set -e
-image=$1
+var=$1
 CONTAINER_ID=$2
 
- sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} dtdwd/${image}
+image=${var,,}
+ctx logger info "Creating ${image}"
+if [[ "$(docker images -q dtdwd/${image} 2> /dev/null)" = "" ]]; then
+   sudo docker commit -m "new ${image} image" -a "rawa" ${CONTAINER_ID} dtdwd/${image}
+fi
