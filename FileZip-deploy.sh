00dbc2fb53651c88136f38f612f0b38bf6bbd9ad
DTDWD <r.qasha@newcastle.ac.uk>
2016-05-25 17:28:39 +0100

    changing blueprints name

--- FileZip-deploy.sh
+++ FileZip-deploy.sh
@@ -21,17 +21,17 @@ sudo service docker start
 
 echo "deploy the workflow"
 if [[ ${wf} == 1 ]]; then
-   if [[ -d ~/FileZip-WF ]]; then
+   if [[ -d ~/FileZip ]]; then
       echo "previous workflow execution exists and will be deleted"
-      rm -rf ~/FileZip-WF 
+      rm -rf ~/FileZip 
    fi
-  cfy local init --install-plugins -p FileZip-WF.yaml --input input.yaml
+  cfy local init --install-plugins -p FileZip.yaml --input input.yaml
 else
-   if [[ -d ~/FileZip-1host  ]]; then
+   if [[ -d ~/FileZip-1container  ]]; then
       echo "previous workflow execution exists and will be deleted"
-      rm -rf ~/FileZip-1host
+      rm -rf ~/FileZip-1container
    fi
-  cfy local init --install-plugins -p FileZip-1host.yaml -i input.yaml
+  cfy local init --install-plugins -p FileZip-1container.yaml -i input.yaml
 fi
 
 cfy local execute -w install
